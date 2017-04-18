2eaf10baf54aa639dbe68deb4ff33194359f4ac5
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    tama-common-kddi: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,4 +186,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/sony/tama-common-kddi/BoardConfigVendor.mk
+include vendor/sony/tama-common-kddi/BoardConfigVendor.mk
