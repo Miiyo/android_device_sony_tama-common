e268f06cefb87c336e946a578e5a53516e0648d1
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2020-02-12 01:51:30 +0000

    tama-common-kddi: Enable Freeform Multiwindow
    Change-Id: I82965ce0c285ef52bc44b0745ec7c2b0007ec66d

--- common.mk
+++ common.mk
@@ -259,6 +259,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0.vendor
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
