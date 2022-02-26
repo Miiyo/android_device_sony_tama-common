b43c8836df9f7c186beadec99aa9d8903212e804
PainKiller3 <ninadpatil100@gmail.com>
2022-02-27 01:05:16 +0530

    tama-common-kddi: Configure cpusets for dex2oat
     * use all 8 cpus for all operations
     * https://source.android.com/devices/tech/dalvik/configure
    
    Change-Id: I202e425b2aa752b9e6115938bff6051907c8ee57
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- common.mk
+++ common.mk
@@ -188,6 +188,15 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     ueventd.tama.rc
 
+# Configure dex2oat
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+    
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
