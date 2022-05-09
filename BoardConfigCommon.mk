05cf561528a5ff45c3e0d04023893a863e0b3435
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-05-10 00:57:32 +0600

    tama-common-kddi: Set up HIDL matrix from QCOM common
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: Idae7e57866599697a8a32c8b7ea1bea8d11c506c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,7 +112,7 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
