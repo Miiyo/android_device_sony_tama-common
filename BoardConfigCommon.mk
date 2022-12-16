213c4e1ee648af41c50f2ae1cad31d312bd9c18b
nullbytepl <nullbytepl@gmail.com>
2022-12-16 21:16:08 +0530

    tama-common-kddi: Disable kpti
     * It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += panic_on_err=1
