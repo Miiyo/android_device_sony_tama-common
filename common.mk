b21fbfb411f876f8d47b8085dde95532968619c9
ekkusa <dayesofficial@gmail.com>
2023-02-25 13:59:35 +0000

    tama-common-kddi: Import CXD-224 NFC
    * Required for NFC on Japanese devices.

--- common.mk
+++ common.mk
@@ -317,14 +317,13 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/configs/init.tama_kddi.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.tama_kddi.nfc.rc
 
 # Perfd (dummy)
 PRODUCT_PACKAGES += \
