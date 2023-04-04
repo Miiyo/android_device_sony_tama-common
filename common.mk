474a5206c8872e4eddc9d9775fac46d775a5f729
ekkusa <dayesofficial@gmail.com>
2023-04-04 20:12:54 +0800

    tama-common-kddi: Build Radio 1.6

--- common.mk
+++ common.mk
@@ -395,7 +395,11 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.6 \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3 \
     android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     librmnetctl
