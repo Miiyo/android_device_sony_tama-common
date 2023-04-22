05440e3586ef7cd8bfb273212d63d24b30175939
ekkusa <dayesofficial@gmail.com>
2023-04-23 04:04:15 +0800

    REVERT: tama-common-kddi: build a2dp service and impl
    This reverts commit 17c93c7f08c3b913da76cf21f7edb31c246d84d5.

--- common.mk
+++ common.mk
@@ -149,9 +149,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-service
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Boot control
 PRODUCT_PACKAGES += \
